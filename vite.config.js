67635aa4ca888fb714b2206ebc14ab78dda13410
sainadh_garlapati <snadh843@gmail.com>
2026-09-10 21:49:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/quantumrealm/',
   plugins: [react()],
   server: {
     host: true,
